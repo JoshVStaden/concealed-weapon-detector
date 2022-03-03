8d27cee70b801db1746deb9183a9ace3dd9b648d
Joshua van Staden <g14v2805@campus.ru.ac.za>
2022-03-03 13:05:40 +0200

    Refactor and add documentation

--- process_keypoints.py
+++ process_keypoints.py
@@ -6,36 +6,53 @@ import config
 from yolo import GUN_DETECTION_MODEL
 from util.util import image_object_detection
 
-# person_id frame_number x y score
-
 KEYPOINT_FILE_FORMAT_STRING = "keypoints/{}.txt"
 VIDEO_FORMAT_STRING = "../../Datasets/MoviesGuns/{}.mp4"
 
 
 def read_keypoints_file(vid_number):
+    """
+    Reads and returns keypoints from keypoints/<vid_number>.txt
+    Keypoints are stored as "person_id frame_no x y confidence"
+
+    Returns: Dataframe of all keypoints
+    """
+    # Read in keypoints as keypoints/<vid_number>.txt
     keypoints_file = open(KEYPOINT_FILE_FORMAT_STRING.format(vid_number))
 
+    # Individual people and frames are separated by blank lines
     full_text = keypoints_file.read()
-
     individual_people = full_text.split("\n\n")
 
     keypoints_dict = {'person_id': [], 'frame': [], 'xyc': []}
-
     for keypoint_set in individual_people:
+        # Skip any blank lines
         if keypoint_set == "":
             continue
+
+        # Extract all keypoints
         ind_kp = keypoint_set.split("\n")
         for point in ind_kp:
+            # Convert to float, round to nearest decimal, convert to int
             p_id, frame, x, y = map(lambda x: int(round(float(x))),
                                     point.split(" ")[:-1])
+            
+            # Only convert confidence to float
             conf = float(point.split(" ")[-1])
+
+            # Add to dictionary
             keypoints_dict['person_id'].append(p_id)
             keypoints_dict['frame'].append(frame)
             keypoints_dict['xyc'].append((x, y, conf))
+
+    # Return dataframe
     return pd.DataFrame(keypoints_dict)
 
 
 def snap_to_frame(coords, frame_shape):
+    """
+    Snap all coordinates within frame
+    """
     if coords[0] < 0:
         coords = (0, coords[1])
 
@@ -52,22 +69,32 @@ def snap_to_frame(coords, frame_shape):
 
 
 def extract_hand_regions(person_keypoints, frame_shape=(640, 480)):
-    
+    """
+    Searches for the hands, and returns regions around the hands
+
+    Returns: [(xmin, ymin),(xmax, ymax)], a bounding box around the hands
+    """
+    # Looks for right wrist and right elbow
+    # Only returns them if the confidence values are more than 0
     if person_keypoints.xyc.values[config.RIGHT_WRIST][2] > 0 and person_keypoints.xyc.values[config.RIGHT_ELBOW][2] > 0:
         right_wrist = person_keypoints.xyc.values[config.RIGHT_WRIST][:2]
         right_elbow = person_keypoints.xyc.values[config.RIGHT_ELBOW][:2]
     else:
         return []
     
+    # Find distance from forearm to wrist
     forearm_length = np.sqrt((right_wrist[0] - right_elbow[0]) ** 2 + (right_wrist[1] - right_elbow[1]) ** 2)
 
+    # If the forearm is too small, don't return anything
     if forearm_length > 20:
 
+        # Snap coordinates within frame
+        # Top left of box is defined with forearm length
         top_left = (int(right_wrist[0] - forearm_length), int(right_wrist[1] - forearm_length))
         top_left = snap_to_frame(top_left, frame_shape)
 
-        # bottom_right = (int(right_wrist[0] + forearm_length), int(right_wrist[1] + forearm_length))
-        bottom_right = (int(right_wrist[0] + forearm_length // 3), int(right_wrist[1] + forearm_length // 3))
+        # Bottom right of box is defined with forearm length divided by 3
+        bottom_right = (int(right_wrist[0] + forearm_length), int(right_wrist[1] + forearm_length))
         bottom_right = snap_to_frame(bottom_right, frame_shape)
 
         return [top_left, bottom_right]
@@ -76,53 +103,108 @@ def extract_hand_regions(person_keypoints, frame_shape=(640, 480)):
 
 
 def hand_coords_to_image_coords(hand_coords_start, hand_box_size, hand_im_shape=(128, 128)):
+    """
+    Based on the size of the hand's bounding box, as well as the resized image, rescale
+    the hand-coordinates to fit into the image.
+
+    Returns: function(gun_coords) => corrected_coords: a function that converts the coordinates of the gun 
+             to image coordinates
+    """
+    # Extract top left of hand's bounding box
     x_start, y_start = hand_coords_start
+
+    # Rescale according to bounding box size and image size
     x_scale = hand_box_size[1] / hand_im_shape[1]
     y_scale = hand_box_size[0] / hand_im_shape[0]
+
+
     def _return_fun(gun_coords):
         ret_arr = []
+
+        # Iterate over gun_coords
         for [(x_gun_min, y_gun_min), (x_gun_max, y_gun_max)] in gun_coords:
+            # Rescale coords
             scaled_x_min = int(x_gun_min * x_scale)
             scaled_y_min = int(y_gun_min * y_scale)
             scaled_x_max = int(x_gun_max * x_scale)
             scaled_y_max = int(y_gun_max * y_scale)
+
+            # Add to top left to fit in image
             ret_arr.append([(x_start + scaled_x_min, y_start + scaled_y_min), (x_start + scaled_x_max, y_start + scaled_y_max)])
         return ret_arr
     return _return_fun
         
+def annotate_keypoints(frame, person_keypoints):
+    """
+    Take single person's keypoints and annotate image
+
+    Returns: image annotation
+    """
+    for coords in person_keypoints.xyc:
+        x, y, conf = coords
+        frame = cv2.circle(frame, (x, y), 5, (255, 0, 0), -1)
+
+    return frame
 
 
-def annotate_image(frame, keypoints):
+
+def annotate_image(frame, keypoints, annotate_kp=False, annotate_hand_bbox=False, annotate_gun=True, show_hand_image=True):
+    """
+    Takes in keypoints and an image, and annotates it.
+    Current annotations: keypoints, right hand bounding box, gun found within right hand
+    Keypoints must only contain those within the current frame
+
+    Returns: the annotated image
+    """
+    # Get all people within current frame
     people = np.unique(keypoints.person_id)
-    curr_kp = 0
+
+    # Iterate over people
     for person in people:
+        # Get current person's keypoints
         person_keypoints = keypoints[keypoints.person_id == person]
+
+        # Find their right hand
         hand_regions = extract_hand_regions(person_keypoints)
-        # gun_coords = []
-        # print(top_left, bottom_right)
+
+        # If hand not visible, skip
         if len(hand_regions) > 0:
+            # Extract the portion of the image containing the right hand
             top_left, bottom_right = hand_regions
             hand_image = frame[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-            
-            frame = cv2.rectangle(frame, top_left, bottom_right, (255, 0, 0), 2)
 
+            # Draw rectangle around right hand
+            if annotate_hand_bbox:
+                frame = cv2.rectangle(frame, top_left, bottom_right, (255, 0, 0), 2)
+
+            # Find the size of the bbox around the hand
             hand_size = (bottom_right[0] - top_left[0], bottom_right[1] - top_left[1])
+            print(hand_size)
 
-            hand_image = cv2.resize(hand_image, (128, 128))
-            gun_to_image = hand_coords_to_image_coords(top_left, hand_size, hand_im_shape=(128, 128))
+            # Resize hand image to (128, 128)
+            hand_image = cv2.resize(hand_image, (64, 64))
+
+            # Get a converter to global coordinates
+            gun_to_image = hand_coords_to_image_coords(top_left, hand_size, hand_im_shape=(64, 64))
             hand_image = cv2.cvtColor(hand_image, cv2.COLOR_BGR2RGB)
+
+            # Detect any guns within the image of the hand
             gun_coord, hand_image = image_object_detection(GUN_DETECTION_MODEL, hand_image)
-            if len(gun_coord) > 0:
+
+            # If no guns, skip
+            if annotate_gun and len(gun_coord) > 0:
+                # Get coordinates, convert to global coords, draw rectange around gun
                 min_gun, max_gun = gun_to_image(gun_coord)[0]
-                print(min_gun, max_gun)
                 frame = cv2.rectangle(frame, min_gun, max_gun, (255,255,255))
-            cv2.imshow("Hand", hand_image)
-        # print(len(person_keypoints.xyc))
-        for coords in person_keypoints.xyc:
-            x, y, conf = coords
-            if curr_kp == 100:
-                frame = cv2.circle(frame, (x, y), 5, (255, 0, 0), -1)
-            curr_kp += 1
+            
+            # Show a separate image with the hand
+            if show_hand_image:
+                cv2.imshow("Hand", hand_image)
+        
+        # Annotate keypoints
+        if annotate_kp:
+            frame = annotate_keypoints(frame, person_keypoints)
+            
     return frame
 
 
@@ -134,9 +216,6 @@ def process_single_video(vid_number):
     cap = cv2.VideoCapture(VIDEO_FORMAT_STRING.format(vid_number))
 
     keypoints = read_keypoints_file(vid_number)
-    # print(keypoints)
-    # quit()
-
     frame_no = 0
 
     # Iterate over frames in video
--- util/util.py
+++ util/util.py
@@ -1,7 +1,15 @@
 from unittest import result
 import cv2
+import numpy as np
 
 def image_object_detection(model, frame, annotate=True):
+    # brightnesses = np.sum(frame, axis=2) / (255)
+    # frame[np.where(brightnesses < 0.1),:] = 0
+    # frame = frame * 255
+    # brightnesses = np.sum(frame, axis=0)
+    # print(np.min(frame), np.max(frame))
+    # quit()
+    # frame[np.where(brightnesses < 0.2), :] = 0
     results = model([frame]).pandas().xyxy[0]
 
     coords = []
@@ -9,18 +17,18 @@ def image_object_detection(model, frame, annotate=True):
     if len(conf_scores) == 0:
         return (coords, frame)
     max_conf = max(conf_scores)
-    for idx, res in results.iterrows():
-        print("Found a GUN!")
-        if res.confidence == max_conf:
-            xmin = int(res.xmin)
-            ymin = int(res.ymin)
-            xmax = int(res.xmax)
-            ymax = int(res.ymax)
-            coords.append([(xmin, ymin), (xmax, ymax)])
-            if annotate:
-                frame = cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255,255,255))
-                frame = cv2.putText(frame, f"{res['name']} {round(res.confidence, 2)}", (xmin, ymin), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255))
-            break
+    if max_conf > 0.0:
+        for idx, res in results.iterrows():
+            if res.confidence == max_conf:
+                xmin = int(res.xmin)
+                ymin = int(res.ymin)
+                xmax = int(res.xmax)
+                ymax = int(res.ymax)
+                coords.append([(xmin, ymin), (xmax, ymax)])
+                if annotate:
+                    frame = cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255,255,255))
+                    frame = cv2.putText(frame, f"{res['name']} {round(res.confidence, 2)}", (xmin, ymin), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255))
+                break
     return (coords, frame)
 
 
